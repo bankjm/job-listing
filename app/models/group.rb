092f9879926ffe4039e59b7130a814401f34b676
bankjm <bankjm919@gmail.com>
2017-01-09 13:24:20 +0800

    rails console

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,7 @@
 class Group < ActiveRecord::Base
-  belongs_to :user 
+  belongs_to :user
   validates :title, presence: true
+
+  has_many :group_relationships
+  has_many :members, through: :group_relationships, source: :uesr
 end

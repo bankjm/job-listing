6a0146d74dab801904ecd51a13fbf257830a1c09
bankjm <bankjm919@gmail.com>
2017-01-09 18:18:03 +0800

    add post and hook group/user relation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170109051555) do
+ActiveRecord::Schema.define(version: 20170109101440) do
 
   create_table "group_relationships", force: :cascade do |t|
     t.integer  "group_id"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 20170109051555) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "group_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
